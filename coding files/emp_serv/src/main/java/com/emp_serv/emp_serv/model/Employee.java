f2f0109f006616921075c315cc9e7d9734f2c315
mohamedthegreat <76401697+mohamedthegreat@users.noreply.github.com>
2023-04-28 16:56:32 -0400

    First commit

---
+++ coding files/emp_serv/src/main/java/com/emp_serv/emp_serv/model/Employee.java
@@ -0,0 +1,63 @@
+package com.emp_serv.emp_serv.model;
+import javax.persistence.*;
+
+@Entity
+@Table(name="emp")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    //Checking columns of database
+    @Column(name="first_name")
+    private String firstName;
+    @Column(name="last_name")
+    private String lastName;
+    @Column(name="email")
+    private String email;
+
+    @Column(name = "phone_number")
+    public String phoneNumber;
+
+
+//getters and setters
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
